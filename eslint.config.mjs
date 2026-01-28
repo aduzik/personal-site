53aee48ada29f595439e7be4feab8fe7a93a6738
Alex Duzik <alex@aduzik.com>
2026-01-27 18:47:59 -0600

    Lint Tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tailwind from 'eslint-plugin-tailwindcss';
+import path from "path";
 
 const eslintConfig = defineConfig([
     ...nextVitals,
     ...nextTs,
+    ...tailwind.configs['flat/recommended'],
     // Override default ignores of eslint-config-next.
     globalIgnores([
         // Default ignores of eslint-config-next:
@@ -13,6 +16,13 @@ const eslintConfig = defineConfig([
         "build/**",
         "next-env.d.ts",
     ]),
+    {
+        settings: {
+            tailwindcss: {
+                config: path.join(process.cwd(), "tailwind.config.mjs"),
+            },
+        },
+    }
 ]);
 
 export default eslintConfig;
