2ce551cef2c97f5e3b438988133ca97fa1584c4e
Alex Duzik <alex@aduzik.com>
2026-02-02 23:42:45 -0600

    Simplify navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,8 +1,7 @@
-import React, { useCallback, useContext, useEffect, useImperativeHandle, useRef, useState } from "react";
+import React, { useCallback, useEffect, useImperativeHandle, useRef, useState } from "react";
 import {
   FloatingPortal,
   offset,
-  size,
   useClick,
   useDismiss,
   useFloating,
@@ -14,64 +13,26 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { twJoin, twMerge } from "tailwind-merge";
 
-export type NavBarProps = {
-  headerRef?: React.RefObject<HTMLElement | null>;
-};
-
-const NavBarContext = React.createContext({
-  responsive: false,
-});
-
-export default function NavBar({ headerRef }: NavBarProps) {
+export default function NavBar() {
   const [open, setOpen] = useState(false);
-  const [responsive, setResponsive] = useState(false);
-  const [navHeight, setNavHeight] = useState(0);
   const navRef = useRef<HTMLElement>(null);
-
-  useEffect(() => {
-    const mediaQueryList = window.matchMedia("(min-width: 768px)");
-
-    setTimeout(() => {
-      setResponsive(!mediaQueryList.matches);
-    }, 0);
-
-    const onChange = (e: MediaQueryListEvent) => {
-      setResponsive(!e.matches);
-    };
-    mediaQueryList.addEventListener("change", onChange);
-
-    return () => {
-      mediaQueryList.removeEventListener("change", onChange);
-    };
-  }, []);
 
   const {
     context,
-    refs: { setReference, setFloating, setPositionReference },
-    floatingStyles,
-    elements: { floating },
+    refs: { setReference, setFloating },
   } = useFloating<HTMLDivElement>({
     placement: "bottom",
     strategy: "fixed",
     transform: false,
     open,
     onOpenChange: setOpen,
-    middleware: [
-      size({
-        apply({ availableWidth, elements }) {
-          elements.floating.style.setProperty("--available-width", `${availableWidth}px`);
-        },
-      }),
-    ],
   });
 
   const click = useClick(context, {
     toggle: true,
-    enabled: responsive,
   });
 
   const dismiss = useDismiss(context, {
-    enabled: responsive,
     outsidePress: true,
     escapeKey: true,
     ancestorScroll: true,
@@ -79,43 +40,37 @@ export default function NavBar({ headerRef }: NavBarProps) {
 
   const { getReferenceProps, getFloatingProps } = useInteractions([click, dismiss]);
 
-  const { isMounted, status } = useTransitionStatus(context, {
+  const { status } = useTransitionStatus(context, {
     duration: 750,
   });
 
   useEffect(() => {
-    const header = headerRef?.current;
-    if (!header) return;
+    const nav = navRef.current;
+    if (!nav) return;
 
-    const resizeObserver = new ResizeObserver((entries) => {
-      setPositionReference({
-        getBoundingClientRect() {
-          return entries[0].contentRect;
-        },
-      });
+    const stylesheet = document.createElement("style");
+    document.head.appendChild(stylesheet);
+
+    const updateNavHeight = () => {
+      const navHeight = nav.getBoundingClientRect().height;
+      console.log("navHeight", navHeight);
+      stylesheet.textContent = `:root { --nav-height: ${navHeight}px; }`;
+    };
+
+    const resizeObserver = new ResizeObserver(() => {
+      updateNavHeight();
     });
 
-    resizeObserver.observe(header);
-
-    return () => {
-      resizeObserver.unobserve(header);
-    };
-  }, [headerRef, setPositionReference]);
-
-  useEffect(() => {
-    const nav = navRef.current;
-    if (!floating || !nav || !isMounted) return;
-
-    const resizeObserver = new ResizeObserver((entries) => {
-      const navHeight = entries[0].contentRect.height;
-      setNavHeight(navHeight);
+    requestAnimationFrame(() => {
+      updateNavHeight();
     });
 
     resizeObserver.observe(nav);
     return () => {
       resizeObserver.unobserve(nav);
+      stylesheet.remove();
     };
-  }, [floating, isMounted]);
+  }, []);
 
   useEffect(() => {
     const nav = navRef.current;
@@ -134,57 +89,43 @@ export default function NavBar({ headerRef }: NavBarProps) {
     if (nav.contains(event.target as Node)) setOpen(false);
   }, []);
 
-  const navBarContent: React.ReactNode = (
-    <div
-      ref={setFloating}
-      {...(responsive ? getFloatingProps() : {})}
-      style={
-        responsive ?
-          ({
-            ...floatingStyles,
-            "--nav-height": `${navHeight}px`,
-          } as React.CSSProperties)
-        : undefined
-      }
-      className={twJoin(
-        "group/nav z-10 w-(--available-width) overflow-hidden bg-white/50 shadow-lg backdrop-blur-lg dark:bg-black/10",
-        "md:w-auto md:bg-transparent md:shadow-none md:backdrop-blur-none dark:md:bg-transparent",
-        "h-0 transition-[height] duration-750 data-[transition=closed]:h-0 data-[transition=open]:h-(--nav-height) md:h-auto",
-      )}
-      data-transition={status}
-    >
-      <nav
-        className="overflow-visible"
-        ref={navRef}
-        style={
-          {
-            "--index": "0",
-          } as React.CSSProperties
-        }
-        onClick={onClick}
-      >
-        <ul className="block md:flex md:flex-row md:gap-x-1">
-          <li>
-            <NavBarLink href="/articles" text="Articles" tooltipContent="Read my latest articles" />
-          </li>
-          <li>
-            <NavBarLink href="/about" text="About" tooltipContent="Learn more about me" />
-          </li>
-          <li>
-            <NavBarLink href="/contact" text="Contact" tooltipContent="Get in touch with me" />
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-
   return (
-    <NavBarContext.Provider value={{ responsive }}>
-      <div className="mx-2">
-        <HamburgerButton className="md:hidden" ref={setReference} {...getReferenceProps()} expanded={open} />
-        {responsive ? isMounted && <FloatingPortal>{navBarContent}</FloatingPortal> : navBarContent}
+    <div className="mx-2">
+      <HamburgerButton className="md:hidden" ref={setReference} {...getReferenceProps()} expanded={open} />
+      <div
+        ref={setFloating}
+        {...getFloatingProps()}
+        className={twJoin(
+          "group/nav fixed top-(--site-header-height) right-0 left-0 z-20 overflow-hidden bg-white/50 backdrop-blur-lg dark:bg-black/10",
+          "md:static md:w-auto md:bg-transparent md:shadow-none md:backdrop-blur-none dark:md:bg-transparent",
+          "h-0 transition-[height] duration-750 data-[transition=closed]:h-0 data-[transition=open]:h-(--nav-height) md:h-auto",
+        )}
+        data-transition={status}
+      >
+        <nav
+          className="overflow-visible"
+          ref={navRef}
+          style={
+            {
+              "--index": "0",
+            } as React.CSSProperties
+          }
+          onClick={onClick}
+        >
+          <ul className="block md:flex md:flex-row md:gap-x-1">
+            <li>
+              <NavBarLink href="/articles" text="Articles" tooltipContent="Read my latest articles" />
+            </li>
+            <li>
+              <NavBarLink href="/about" text="About" tooltipContent="Learn more about me" />
+            </li>
+            <li>
+              <NavBarLink href="/contact" text="Contact" tooltipContent="Get in touch with me" />
+            </li>
+          </ul>
+        </nav>
       </div>
-    </NavBarContext.Provider>
+    </div>
   );
 }
 
@@ -196,7 +137,6 @@ type NavBarLinkProps = {
 
 function NavBarLink({ href, text, tooltipContent }: NavBarLinkProps) {
   const [open, setOpen] = useState(false);
-  const { responsive } = useContext(NavBarContext);
   const {
     refs: { setReference, setFloating },
     floatingStyles,
@@ -211,7 +151,6 @@ function NavBarLink({ href, text, tooltipContent }: NavBarLinkProps) {
   });
 
   const hover = useHover(context, {
-    enabled: !responsive,
     restMs: 200,
     delay: {
       close: 500,
@@ -229,7 +168,7 @@ function NavBarLink({ href, text, tooltipContent }: NavBarLinkProps) {
   return (
     <span
       className={twJoin(
-        "relative block leading-none md:mr-4 md:inline-block md:py-0",
+        "relative block px-4 leading-none md:mr-4 md:inline-block md:px-0 md:py-0",
         "-translate-y-4 opacity-0 transition-[opacity,transform,translate,rotate,scale] duration-500 ease-out group-data-[transition=open]/nav:translate-y-0 group-data-[transition=open]/nav:opacity-100 group-data-[transition=open]/nav:delay-[calc(var(--index)*150ms+250ms)]",
         "md:translate-y-0 md:opacity-100",
       )}
