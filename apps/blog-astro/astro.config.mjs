2706fe789eaadea67e96bb6bbc6003accda11cde
Alex Duzik <alex@aduzik.com>
2026-02-25 21:56:05 -0600

    Add heading links

--- apps/blog-astro/astro.config.mjs
+++ apps/blog-astro/astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
 import remarkMath from "remark-math";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeKatex from "rehype-katex";
+import rehypeSlug from "rehype-slug";
 import mdx from "@astrojs/mdx";
 import rehypeSectionize from "@hbsnow/rehype-sectionize";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+
+/** @type{import('rehype-autolink-headings').Options} */
+const headingsOptions = {
+  behavior: "wrap",
+  properties: {
+    dataHeadingLink: true,
+  },
+};
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +22,7 @@ export default defineConfig({
   integrations: [
     mdx({
       remarkPlugins: [remarkMath],
-      rehypePlugins: [rehypeKatex, rehypeSectionize],
+      rehypePlugins: [rehypeKatex, rehypeSectionize, rehypeSlug, [rehypeAutolinkHeadings, headingsOptions]],
     }),
   ],
 
